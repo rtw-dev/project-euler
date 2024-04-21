62555de858620c60d0259942c45be0c388b6eccd
Jacob Conrad Martin <arctangent@gmail.com>
2024-04-21 21:14:21 +0100

    Answer 0001

--- 0001.rs
+++ 0001.rs
@@ -1,1 +1,13 @@
-
+fn main() {
+    let mut sum = 0;
+    for n in 1..1000 {
+        if n % 3 == 0 {
+            // println!("{}", n);
+            sum = sum + n;
+        } else if n % 5 == 0 {
+            //println!("{}", n);
+            sum = sum + n;
+        }
+        println!("{}", sum);
+    }
+}}
