d6bcaf1b9f0287fc8663f6e0cb5a978f521dfa2f
Jacob Conrad Martin <arctangent@gmail.com>
2024-05-05 03:51:27 +0000

    Solution for 0010

---
+++ src/bin/0010-jacob.rs
@@ -0,0 +1,50 @@
+// https://projecteuler.net/problem=10
+// Find the sum of all the primes below two million.
+// Answer: 142913828922
+
+extern crate indicatif;
+use indicatif::ProgressBar;
+
+const BOUND: i64 = 2000000;
+const PROGRESS_BAR_WIDTH: i64 = 50;
+
+fn main() {
+    /*
+    Strategy: Will use same method as in solution for problem 7 to generate the primes
+    */
+    // We need to start with the first prime
+    let mut primes: Vec<i64> = vec![2];
+    // We will start with the integer larger than our cuurent largest prime
+    let mut number: i64 = 3;
+    // Loop until we have the primes up to 2 million
+    let pb = ProgressBar::new(BOUND as u64);
+    loop {
+        if number % PROGRESS_BAR_WIDTH == 0 {
+            pb.inc(PROGRESS_BAR_WIDTH as u64);
+        }
+        // If we have the primes up to 2 million, then we can break out of the loop
+        if number > BOUND {
+            break;
+        }
+        // Check if the current number is prime by trial division
+        let mut is_prime = true;
+        // We don't include the first prime (1) because it divides into every number
+        for prime in &primes {
+            // If any known prime divides the current number, then it is not prime
+            if number % prime == 0 {
+                is_prime = false;
+                break; // No need to check if any more known primes divide into the number
+            }
+        }
+        // If the number is prime, then add it to our list of primes
+        if is_prime {
+            //dbg!(number);
+            primes.push(number);
+        }
+        // Increment the number to test
+        number += 1;
+    }
+    // Output our answer
+    pb.finish_with_message("done");
+    println!("Simple solution: {}", primes.iter().sum::<i64>());
+}
